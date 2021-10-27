f0ff7c0bf608b4e92e23a3877c845e3588be97c9
bar10dr <bar10dr@gmail.com>
2021-10-27 12:07:43 +0200

    Fixes

--- Panthura/main.cpp
+++ Panthura/main.cpp
@@ -1,1 +1,12 @@
-#include "main.h"
+#include "console_extension.h"
+
+using namespace Extensions;
+
+int main()
+{
+	console_extension console;
+
+	console.print_text("Panthaura rocks!");
+
+	return 0;
+};
--- Panthura/main.h
+++ Panthura/main.h
@@ -1,13 +1,4 @@
 #include "console_extension.h"
 
-using namespace Extensions;
 
-int main()
-{
-	console_extension console;
-
-	console.print_text("Panthaura rocks!");
-
-	return 0;
-};
 
