1f2cc82817e5aac15e308b611beb3da0243c62ef
bar10dr <bar10dr@gmail.com>
2021-10-27 12:09:20 +0200

    Changed prama for ifndef

--- Panthura/console_extension.h
+++ Panthura/console_extension.h
@@ -1,11 +1,15 @@
-#pragma once
 #include <stdio.h>
 
 namespace Extensions
 {
+	#ifndef CONSOLE_EXTENSION_H
+	#define CONSOLE_EXTENSION_H
+
 	class console_extension
 	{
 	public:
 		void print_text(char const* text);
 	};
+
+	#endif
 }
