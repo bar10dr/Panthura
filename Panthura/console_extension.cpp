0527f5b1ca9cf46feec44b79a6e4ba63bdb489a2
bar10dr <bar10dr@gmail.com>
2021-10-27 12:02:17 +0200

    Add project files.

---
+++ Panthura/console_extension.cpp
@@ -0,0 +1,8 @@
+#include "console_extension.h"
+
+using namespace Extensions;
+
+void console_extension::print_text(char const* text)
+{
+	printf(text);
+}
---
+++ Panthura/console_extension.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+
+namespace Extensions
+{
+	class console_extension
+	{
+	public:
+		void print_text(char const* text);
+	};
+}
