c7c225a154d8a46929132927cffd3f042b6af3b8
bar10dr <bar10dr@gmail.com>
2021-10-27 12:25:12 +0200

    Formatting

--- Panthura/printText.h
+++ Panthura/printText.h
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-	#ifndef CONSOLE_EXTENSION_H
-	#define CONSOLE_EXTENSION_H
+#ifndef CONSOLE_EXTENSION_H
+#define CONSOLE_EXTENSION_H
 
-	class print_text
-	{
-	public:
-		print_text(char const* text);
-	};
+class print_text
+{
+public:
+	print_text(char const* text);
+};
 
-	#endif
+#endif
